6140344ee62805ea749f7a539b5aedb7c4e849f9
finallymeetyou <2543966939@qq.com>
2023-04-03 21:56:06 +0800

    新增手动延迟，模拟异步

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const mockList = require("./mock/index");
 const app = new Koa();
 const router = new Router();
 
+// 手动延迟
+async function getRes(fn) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = fn();
+      resolve(result);
+    }, 1000);
+  });
+}
+
 mockList.forEach((item) => {
   const { url, method, response } = item;
 
   router[method](url, async (ctx) => {
-    const result = response();
+    const result = await getRes(response);
     ctx.body = result;
   });
 });
